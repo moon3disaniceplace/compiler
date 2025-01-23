e1858ce584203426b5f58c244bbca3c38f2c8e8a
Shimoniohad <60567624+moon3disaniceplace@users.noreply.github.com>
2025-01-23 12:27:01 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "output.hpp"
+#include "nodes.hpp"
+#include "output_ast.cpp"
+#include "semantic.hpp"
+#include <iostream>
+// Extern from the bison-generated parser
+extern int yyparse();
+
+extern std::shared_ptr<ast::Node> program;
+
+int main() {
+    // Parse the input. The result is stored in the global variable `program`
+    yyparse();
+
+    // Print the AST using the PrintVisitor
+    output::PrintVisitor printVisitor;
+    program->accept(printVisitor);
+
+    // Semantic analysis
+    SemanticVisitor semanticVisitor;
+    program->accept(semanticVisitor);
+    std::cout << semanticVisitor.printer;
+}
